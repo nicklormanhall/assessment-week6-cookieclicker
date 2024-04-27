60f3d58de2dfa9df54a9f6967a21eaa14b531a72
nicklormanhall <nicklormanhall@gmail.com>
2024-04-27 16:20:22 +0100

    updated cookies per second

--- src/components/CookieClick.jsx
+++ src/components/CookieClick.jsx
@@ -1,3 +1,41 @@
-export default function CookieClick({ count }) {
-  return <h1> Welcome to my new cookie moster {count} </h1>;
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function CookieClick() {
+  const [cookies, setCookies] = useState(0);
+  const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
+
+  useEffect(() => {
+    const myInterval = setInterval(() => {
+      addCookie();
+    }, 1000 / cookiesPerSecond);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [cookiesPerSecond]);
+
+  function addCookie() {
+    setCookies((currentCookies) => {
+      return currentCookies + 1;
+    });
+  }
+
+  function buyUpgrade() {
+    setCookiesPerSecond(cookiesPerSecond);
+    // setCookiesPerSecond(cookiesPerSecond + 1);
+  }
+
+  return (
+    <>
+      <h1>
+        {" "}
+        Mmmmmmmmm You have {cookies} cookies, you have {cookiesPerSecond} new
+        cookie every second
+      </h1>
+      <div className="cookieBtn">
+        <img src="/Cookie.png" alt="Image" onClick={addCookie} />
+      </div>
+    </>
+  );
 }
