a3def163e5b11d2426995f0510c6af53a9816bc8
nicklormanhall <nicklormanhall@gmail.com>
2024-04-27 20:26:44 +0100

    added upgrades

---
+++ src/components/BuyUpgrade.jsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+// export default function BuyUpgrade() {
+//   return (
+//     <div>
+//       <button onClick={buyUpgrade1}>Buy upgrade</button>
+//       <button onClick={buyUpgrade2}>Buy upgrade</button>
+//       <button onClick={buyUpgrade3}>Buy upgrade</button>
+//     </div>
+//   );
+// }
--- src/components/CookieClick.jsx
+++ src/components/CookieClick.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import { useEffect } from "react";
+// import BuyUpgrade from "./BuyUpgrade";
 
 export default function CookieClick() {
   const [cookies, setCookies] = useState(0);
   const [cookiesPerSecond, setCookiesPerSecond] = useState(1);
 
+  const upgradeItems = [
+    { cost: 10, cps: 1, item: "new mixer" },
+    { cost: 100, cps: 10, item: "bigger oven" },
+    { cost: 1000, cps: 100, item: "new baking tools" },
+    { cost: 10000, cps: 1000, item: "cookie press" },
+    { cost: 100000, cps: 10000, item: "cookie factory" },
+  ];
+
   useEffect(() => {
     const myInterval = setInterval(() => {
       addCookie();
+      // }, 1000);
     }, 1000 / cookiesPerSecond);
 
     return () => {
@@ -21,9 +31,10 @@ export default function CookieClick() {
     });
   }
 
-  function buyUpgrade() {
-    setCookiesPerSecond(cookiesPerSecond);
-    // setCookiesPerSecond(cookiesPerSecond + 1);
+  function buyUpgrade(cps, cost) {
+    // setCookiesPerSecond(cookiesPerSecond);
+    setCookiesPerSecond(cookiesPerSecond + cps);
+    setCookies(cookies - cost);
   }
 
   return (
@@ -35,6 +46,19 @@ export default function CookieClick() {
       </h1>
       <div className="cookieBtn">
         <img src="/Cookie.png" alt="Image" onClick={addCookie} />
+      </div>
+      <div className="buyUpgrades">
+        {upgradeItems.map((upgrade, index) => (
+          <button
+            key={index}
+            onClick={() => buyUpgrade(upgrade.cps, upgrade.cost)}
+            disabled={cookies < upgrade.cost}
+          >
+            {cookies < upgrade.cost
+              ? `NOT AVAILABLE - you need ${upgrade.cost - cookies} cookies`
+              : `Buy ${upgrade.item} for ${upgrade.cost}`}
+          </button>
+        ))}
       </div>
     </>
   );
